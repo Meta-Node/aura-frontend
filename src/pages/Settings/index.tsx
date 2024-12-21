f1ec334aa4cdf3ec6a6eaff5435196f6ceda285f
Ali Maktabi <maktabi876@gmail.com>
2024-12-21 15:45:32 +0330

    fixed ordering on setting items

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -1,5 +1,7 @@
 import { BsTwitterX } from 'react-icons/bs';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaDiscord, FaMoon, FaSun } from 'react-icons/fa';
+import { FaHandsHelping } from 'react-icons/fa';
+import { MdOutlineSecurity } from 'react-icons/md';
 import { SiGitbook } from 'react-icons/si';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
@@ -22,33 +24,12 @@ export const Settings = () => {
     <div className="page page__settings dark:text-white w-full pt-4 flex flex-col gap-4">
       <section className="flex flex-col gap-4 w-full">
         <div
-          className="bg-white-90-card cursor-pointer dark:bg-button-primary rounded-lg pl-5 py-3.5 pr-2"
+          className="bg-white-90-card cursor-pointer flex items-center gap-2 dark:bg-button-primary rounded-lg pl-5 py-3.5 pr-2"
           onClick={() => navigate(RoutePath.ROLE_MANAGEMENT)}
         >
+          <MdOutlineSecurity size={20} />
           <p className="font-medium text-[20px]">Role Management</p>
         </div>
-        <div
-          className="bg-white-90-card cursor-pointer dark:bg-button-primary rounded-lg pl-5 py-3.5 pr-2"
-          onClick={() => navigate(RoutePath.ONBOARDING)}
-        >
-          <p className="font-medium text-[20px]">On Boarding</p>
-        </div>
-
-        <div
-          onClick={() =>
-            dispatch(
-              setPrefferedTheme(prefferedTheme === 'dark' ? 'light' : 'dark'),
-            )
-          }
-          className="bg-white-90-card dark:bg-button-primary cursor-pointer rounded-lg pl-5 py-3.5 pr-5 flex items-center justify-between"
-        >
-          <p className="font-medium text-[20px]">Theme</p>
-          <span className="flex items-center gap-2">
-            {prefferedTheme === 'dark' ? <FaMoon /> : <FaSun />}
-            <small>{prefferedTheme.toUpperCase()}</small>
-          </span>
-        </div>
-
         <Link
           target="_blank"
           to="https://brightid.gitbook.io/aura"
@@ -58,6 +39,33 @@ export const Settings = () => {
           <p className="font-medium text-[20px]">Aura Guide</p>
         </Link>
 
+        <div
+          onClick={() =>
+            dispatch(
+              setPrefferedTheme(prefferedTheme === 'dark' ? 'light' : 'dark'),
+            )
+          }
+          className="bg-white-90-card dark:bg-button-primary cursor-pointer rounded-lg pl-5 py-3.5 pr-5 flex items-center justify-between"
+        >
+          <span className="flex items-center gap-2">
+            {prefferedTheme === 'dark' ? (
+              <FaMoon size={20} />
+            ) : (
+              <FaSun size={20} />
+            )}
+            <p className="font-medium text-[20px]">Theme</p>
+          </span>
+          <small>{prefferedTheme.toUpperCase()}</small>
+        </div>
+        <Link
+          target="_blank"
+          to="https://discord.gg/y24xeXq7mj"
+          className="bg-white-90-card dark:bg-button-primary cursor-pointer flex items-center rounded-lg pl-5 py-3.5 gap-2 pr-5"
+        >
+          <FaDiscord size={20} className="w-7 cursor-pointer" />
+          <p className="font-medium text-[20px]">Discord</p>
+        </Link>
+
         <Link
           target="_blank"
           to="https://x.com/brightidproject"
@@ -65,18 +73,13 @@ export const Settings = () => {
         >
           <BsTwitterX size={25} />
         </Link>
-        <Link
-          target="_blank"
-          to="https://discord.gg/y24xeXq7mj"
-          className="bg-white-90-card dark:bg-button-primary cursor-pointer flex items-center rounded-lg pl-5 py-3.5 gap-2 pr-5"
+        <div
+          className="bg-white-90-card cursor-pointer flex items-center gap-2 dark:bg-button-primary rounded-lg pl-5 py-3.5 pr-2"
+          onClick={() => navigate(`${RoutePath.ONBOARDING}?step=1`)}
         >
-          <img
-            src="/assets/images/Shared/discord.svg"
-            alt="discord"
-            className="w-7 cursor-pointer"
-          />
-          <p className="font-medium text-[20px]">Discord</p>
-        </Link>
+          <FaHandsHelping size={20} />
+          <p className="font-medium text-[20px]">Onboarding</p>
+        </div>
 
         {(__DEV__ ||
           process.env.REACT_APP_IS_CYPRESS === 'true' ||
