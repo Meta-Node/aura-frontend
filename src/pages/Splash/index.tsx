c9d2203760e9178187228082d5aa9fa8257e9075
Ali Maktabi <maktabi876@gmail.com>
2025-01-30 15:27:57 +0330

    added basic buttons for importing data from localstorage

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch } from 'store/hooks';
 import { setSplashScreenShown } from 'store/profile';
 
@@ -6,6 +7,42 @@ import CustomTrans from '../../components/CustomTrans';
 
 const Spalsh = () => {
   const dispatch = useDispatch();
+
+  const [isTabOpen, setIsTabOpen] = useState(false);
+
+  const onCopyDataFromDev = () => {
+    console.log();
+    const devTab = window.open(
+      `${import.meta.env.VITE_DEV_DEPLOYMENT_DOMAIN}/import`,
+      '_blank',
+    );
+
+    devTab?.addEventListener('close', (event) => {
+      setIsTabOpen(false);
+    });
+
+    window.addEventListener('message', (event) => {
+      if (event.origin !== import.meta.env.VITE_DEV_DEPLOYMENT_DOMAIN) return;
+
+      try {
+        const data = JSON.parse(event.data);
+        if (data.type === 'localStorageData') {
+          localStorage.setItem('migratedData', JSON.stringify(data.payload));
+          console.log('Received localStorage data from dev tab.');
+        }
+      } catch (error) {
+        console.error('Invalid message format', error);
+      }
+    });
+
+    setTimeout(() => {
+      devTab?.postMessage(
+        { type: 'requestLocalStorage', secret: 'SHARED_SECRET' },
+        import.meta.env.VITE_DEV_DEPLOYMENT_DOMAIN,
+      );
+    }, 2000);
+  };
+
   return (
     <div className="page page__splash !pt-[90px] !px-[22px] pb-4 flex flex-col">
       <section className="content pl-5 pr-12">
@@ -32,6 +69,14 @@ const Spalsh = () => {
             onClick={() => dispatch(setSplashScreenShown(true))}
           >
             Get Started
+          </button>
+          <button
+            disabled={isTabOpen}
+            className="btn mt-4 text-sm !text-white max-w-[270px] w-full"
+            data-testid="splash-dismiss-btn"
+            onClick={() => onCopyDataFromDev()}
+          >
+            Copy the data [DEV ONLY]
           </button>
         </Scale>
       </section>
