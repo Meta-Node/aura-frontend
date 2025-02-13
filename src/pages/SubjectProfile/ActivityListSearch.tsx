a7e18c0914cd9613c44fc66e495289be19ac6440
Ali Maktabi <maktabi876@gmail.com>
2025-02-13 17:07:29 +0330

    fixed stylings

--- src/pages/SubjectProfile/ActivityListSearch.tsx
+++ src/pages/SubjectProfile/ActivityListSearch.tsx
@@ -210,8 +210,8 @@ export const ActivityListSearch = ({
         </span>
       </div>
 
-      <div className="bg-card text-card-foreground rounded-lg p-1 flex-1 flex flex-col justify-center gap-4 max-h-[175px]">
-        <div className="card__input border flex gap-2 items-center rounded px-3.5">
+      <div className="bg-card border text-card-foreground rounded-lg p-1 flex-1 flex flex-col justify-center gap-4 max-h-[175px]">
+        <div className="card__input flex gap-2 items-center rounded px-3.5">
           <img
             className="w-4 h-4"
             src="/assets/images/Shared/search-icon.svg"
