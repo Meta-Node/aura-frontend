afe047646ce6945481498e73c859500627479d20
Ali Maktabi <maktabi876@gmail.com>
2025-02-25 14:18:13 +0330

    added sekeleton loadings for subject cards

--- src/components/EvaluationFlow/SubjectCard.tsx
+++ src/components/EvaluationFlow/SubjectCard.tsx
@@ -16,21 +16,64 @@ import { HorizontalProgressBar } from '../Shared/HorizontalProgressBar';
 import Tooltip from '../Shared/Tooltip';
 import { Card } from '../ui/card';
 
-export const SubjectCard = ({
-  subjectId,
-  index,
-}: {
+export interface SubjectCardSkeletonProps {
   subjectId: string;
   index?: string | number;
-}) => {
+}
+
+export const SubjectCardSkeleton = ({
+  subjectId,
+  index,
+}: SubjectCardSkeletonProps) => {
+  return (
+    <div data-testid={`subject-card-skeleton-${subjectId}`}>
+      <div className="b-4 flex w-full !flex-row items-center !justify-between gap-1 rounded-lg border bg-card p-4">
+        <div
+          className="evaluation-left flex flex-col gap-2"
+          data-testid={`skeleton-item-${index}`}
+        >
+          <div className="evaluation-left__top flex gap-3">
+            <div className="evaluation__profile">
+              {/* Profile picture skeleton */}
+              <div className="h-12 w-12 animate-pulse rounded-full bg-gray-200 dark:bg-gray-700" />
+            </div>
+
+            <div className="evaluation__info flex flex-col gap-2">
+              {/* Name skeleton */}
+              <div className="h-4 w-32 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+
+              {/* Level and Score skeleton */}
+              <div className="flex flex-col gap-1">
+                <div className="h-4 w-24 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+                <div className="h-4 w-20 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+              </div>
+
+              {/* Progress bar skeleton */}
+              <div className="mt-1 h-2.5 w-36 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+            </div>
+          </div>
+
+          {/* Connection status skeleton */}
+          <div className="mt-auto">
+            <div className="h-4 w-40 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+          </div>
+        </div>
+
+        {/* Chart skeleton */}
+        <div className="evaluation-right flex flex-col items-end">
+          <div className="h-[48px] w-[100px] animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export const SubjectCard = ({ subjectId, index }: SubjectCardSkeletonProps) => {
   const name = useSubjectName(subjectId);
 
   const { currentViewMode, currentEvaluationCategory } = useViewMode();
 
-  const { auraLevel, auraScore, auraImpacts } = useSubjectVerifications(
-    subjectId,
-    currentEvaluationCategory,
-  );
+  const { auraLevel, auraScore, auraImpacts, loading } =
+    useSubjectVerifications(subjectId, currentEvaluationCategory);
 
   const { impactChartSmallOption } = useImpactEChartOption(auraImpacts);
 
@@ -38,6 +81,9 @@ export const SubjectCard = ({
     currentEvaluationCategory,
     auraScore ?? 0,
   );
+
+  if (loading)
+    return <SubjectCardSkeleton index={index} subjectId={subjectId} />;
 
   return (
     <Link
--- src/components/Shared/EmptyAndLoadingStates/LoadingList.tsx
+++ src/components/Shared/EmptyAndLoadingStates/LoadingList.tsx
@@ -1,11 +1,45 @@
 export const LoadingList = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-10 px-2 gap-2">
-      <div className="flex gap-1 items-center">
-        <h2 className="text-xl font-medium text-white">Loading</h2>
-        <span className="mt-2 w-1 h-1 bg-white rounded-full animate-bounce"></span>
-        <span className="mt-2 w-1 h-1 bg-white rounded-full animate-bounce animation-delay-200"></span>
-        <span className="mt-2 w-1 h-1 bg-white rounded-full animate-bounce animation-delay-400"></span>
+    <div className="flex flex-col gap-4">
+      {Array.from({ length: 5 }).map((_, key) => (
+        <SimpleSubjectCardSkeleton key={key} />
+      ))}
+    </div>
+  );
+};
+
+export const SimpleSubjectCardSkeleton = () => {
+  return (
+    <div className="b-4 flex w-full !flex-row items-center !justify-between gap-1 rounded-lg border bg-card p-4">
+      <div className="evaluation-left flex flex-col gap-2">
+        <div className="evaluation-left__top flex gap-3">
+          {/* Profile picture skeleton */}
+          <div className="h-12 w-12 animate-pulse rounded-full bg-gray-200 dark:bg-gray-700" />
+
+          <div className="flex flex-col gap-2">
+            {/* Name skeleton */}
+            <div className="h-4 w-32 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+
+            {/* Level and Score skeleton */}
+            <div className="flex flex-col gap-1">
+              <div className="h-4 w-24 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+              <div className="h-4 w-20 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+            </div>
+
+            {/* Progress bar skeleton */}
+            <div className="mt-1 h-2.5 w-36 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+          </div>
+        </div>
+
+        {/* Connection status skeleton */}
+        <div className="mt-2">
+          <div className="h-4 w-40 animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
+        </div>
+      </div>
+
+      {/* Chart skeleton */}
+      <div className="flex flex-col items-end">
+        <div className="h-[48px] w-[100px] animate-pulse rounded-sm bg-gray-200 dark:bg-gray-700" />
       </div>
     </div>
   );
