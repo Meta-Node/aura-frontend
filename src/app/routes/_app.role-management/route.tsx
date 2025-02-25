51ae9a29e3ce19fd1bb16aefae71760656db6f43
Ali Maktabi <maktabi876@gmail.com>
2025-02-25 11:43:28 +0330

    fixed role management page issue

--- src/app/routes/_app.role-management/route.tsx
+++ src/app/routes/_app.role-management/route.tsx
@@ -14,31 +14,35 @@ import { useSubjectVerifications } from 'hooks/useSubjectVerifications';
 import { selectAuthData } from 'store/profile/selectors';
 import { EvaluationCategory } from 'types/dashboard';
 import { compactFormat } from 'utils/number';
+import DefaultHeader from '@/components/Shared/DefaultHeader';
 
-export const RoleManagement = () => {
+export default function RoleManagement() {
   const authData = useSelector(selectAuthData);
   const subjectId = authData!.brightId;
 
   return (
-    <SubjectOutboundEvaluationsContextProvider subjectId={subjectId!}>
-      <SubjectInboundEvaluationsContextProvider subjectId={subjectId!}>
-        <SubjectInboundConnectionsContextProvider subjectId={subjectId!}>
-          <div className="page flex flex-col flex-1">
-            <section className="flex flex-col gap-3">
-              <PlayerCard subjectId={subjectId} />
-              <TrainerCard subjectId={subjectId} />
-              <ManagerCard subjectId={subjectId} />
-            </section>
+    <>
+      <DefaultHeader title="Settings" />
+      <SubjectOutboundEvaluationsContextProvider subjectId={subjectId!}>
+        <SubjectInboundEvaluationsContextProvider subjectId={subjectId!}>
+          <SubjectInboundConnectionsContextProvider subjectId={subjectId!}>
+            <div className="page flex flex-1 flex-col">
+              <section className="flex flex-col gap-3">
+                <PlayerCard subjectId={subjectId} />
+                <TrainerCard subjectId={subjectId} />
+                <ManagerCard subjectId={subjectId} />
+              </section>
 
-            {/* <section className="mt-auto flex w-full justify-center">
+              {/* <section className="mt-auto flex w-full justify-center">
               <p className="text-white text-sm">Aura version 2.1</p>
             </section> */}
-          </div>
-        </SubjectInboundConnectionsContextProvider>
-      </SubjectInboundEvaluationsContextProvider>
-    </SubjectOutboundEvaluationsContextProvider>
+            </div>
+          </SubjectInboundConnectionsContextProvider>
+        </SubjectInboundEvaluationsContextProvider>
+      </SubjectOutboundEvaluationsContextProvider>
+    </>
   );
-};
+}
 
 export type SubjectIdProps = {
   subjectId: string;
@@ -50,17 +54,17 @@ const PlayerCard: FC<SubjectIdProps> = ({ subjectId }) => {
     EvaluationCategory.PLAYER,
   );
   return (
-    <div className="bg-white-90-card dark:bg-button-primary flex flex-col gap-3.5 relative cursor-pointer rounded-lg pl-5 py-[18px] pr-6 pb-4 min-h-[150px]">
+    <div className="relative flex min-h-[150px] cursor-pointer flex-col gap-3.5 rounded-lg bg-white-90-card py-[18px] pb-4 pl-5 pr-6 dark:bg-button-primary">
       <img
         src="/assets/images/RoleManagement/player-shadow-icon.svg"
         alt=""
-        className="absolute top-0 left-0"
+        className="absolute left-0 top-0"
       />
       <section className="flex justify-between">
         <div className="flex gap-2">
           <img src="/assets/images/Shared/player.svg" alt="" />
           <div>
-            <p className="font-medium dark:text-white text-[20px]">Player</p>
+            <p className="text-[20px] font-medium dark:text-white">Player</p>
           </div>
         </div>
         <PlayerLevelAndScore
@@ -71,7 +75,7 @@ const PlayerCard: FC<SubjectIdProps> = ({ subjectId }) => {
         />
       </section>
 
-      <section className="flex dark:text-white text-black justify-between mt-auto"></section>
+      <section className="mt-auto flex justify-between text-black dark:text-white"></section>
     </div>
   );
 };
@@ -91,17 +95,17 @@ const TrainerCard: FC<SubjectIdProps> = ({ subjectId }) => {
   );
 
   return (
-    <div className="bg-white-90-card flex dark:bg-button-primary flex-col gap-3.5 relative cursor-pointer rounded-lg pl-5 py-[18px] pr-6 pb-4 min-h-[150px]">
+    <div className="relative flex min-h-[150px] cursor-pointer flex-col gap-3.5 rounded-lg bg-white-90-card py-[18px] pb-4 pl-5 pr-6 dark:bg-button-primary">
       <img
         src="/assets/images/RoleManagement/trainer-shadow-icon.svg"
         alt=""
-        className="absolute top-0 left-0"
+        className="absolute left-0 top-0"
       />
       <section className="flex justify-between">
-        <div className="flex gap-2 dark:text-white text-black">
+        <div className="flex gap-2 text-black dark:text-white">
           <img src="/assets/images/Shared/trainer.svg" alt="" />
           <div>
-            <p className="font-medium text-[20px] dark:text-white">Trainer</p>
+            <p className="text-[20px] font-medium dark:text-white">Trainer</p>
           </div>
         </div>
         <PlayerLevelAndScore
@@ -114,18 +118,18 @@ const TrainerCard: FC<SubjectIdProps> = ({ subjectId }) => {
       {!playerEvaluation.auraLevel || playerEvaluation.auraLevel < 2 ? (
         <>
           <section>
-            <div className="flex gap-2 items-center mt-2 text-sm font-medium">
+            <div className="mt-2 flex items-center gap-2 text-sm font-medium">
               <img src="/assets/images/RoleManagement/item.svg" alt="" />
 
               <p className="dark:text-white">Reach Player level 2 to unlock</p>
-              <span className="dark:text-white font-bold"> Trainer </span>
+              <span className="font-bold dark:text-white"> Trainer </span>
             </div>
           </section>
         </>
       ) : null}
 
       {!!playerEvaluation.auraLevel && playerEvaluation.auraLevel >= 2 && (
-        <section className="flex justify-end dark:text-white text-black mt-auto">
+        <section className="mt-auto flex justify-end text-black dark:text-white">
           {hasTrainerRole ? (
             <button
               onClick={() => dispatch(toggleTrainerRole())}
@@ -135,7 +139,7 @@ const TrainerCard: FC<SubjectIdProps> = ({ subjectId }) => {
             </button>
           ) : (
             <button
-              className="btn !bg-pl2 btn--small"
+              className="btn btn--small !bg-pl2"
               onClick={() => dispatch(toggleTrainerRole())}
             >
               Show
@@ -163,17 +167,17 @@ const ManagerCard: FC<SubjectIdProps> = ({ subjectId }) => {
   const dispatch = useDispatch();
 
   return (
-    <div className="bg-white-90-card flex dark:bg-button-primary flex-col gap-3.5 relative cursor-pointer rounded-lg pl-5 py-[18px] pr-6 pb-4 min-h-[150px]">
+    <div className="relative flex min-h-[150px] cursor-pointer flex-col gap-3.5 rounded-lg bg-white-90-card py-[18px] pb-4 pl-5 pr-6 dark:bg-button-primary">
       <img
         src="/assets/images/RoleManagement/manager-shadow-icon.svg"
         alt=""
-        className="absolute top-0 left-0"
+        className="absolute left-0 top-0"
       />
       <section className="flex justify-between">
         <div className="flex gap-2">
           <img src="/assets/images/Shared/manager.svg" alt="" />
           <div>
-            <p className="font-medium text-[20px] dark:text-white">Manager</p>
+            <p className="text-[20px] font-medium dark:text-white">Manager</p>
           </div>
         </div>
         <PlayerLevelAndScore
@@ -186,17 +190,17 @@ const ManagerCard: FC<SubjectIdProps> = ({ subjectId }) => {
       {!trainerEvaluation.auraLevel || trainerEvaluation.auraLevel < 1 ? (
         <>
           <section>
-            <div className="flex gap-2 items-center mt-2 text-sm font-medium">
+            <div className="mt-2 flex items-center gap-2 text-sm font-medium">
               <img src="/assets/images/RoleManagement/item.svg" alt="" />
 
               <p className="dark:text-white">Reach Trainer level 1 to unlock</p>
-              <span className="dark:text-white font-bold"> Manager </span>
+              <span className="font-bold dark:text-white"> Manager </span>
             </div>
           </section>
         </>
       ) : null}
       {!!trainerEvaluation.auraLevel && trainerEvaluation.auraLevel > 1 && (
-        <section className="flex justify-end dark:text-white text-black mt-auto">
+        <section className="mt-auto flex justify-end text-black dark:text-white">
           {hasManagerRole ? (
             <button
               onClick={() => dispatch(toggleManagerRole())}
@@ -206,7 +210,7 @@ const ManagerCard: FC<SubjectIdProps> = ({ subjectId }) => {
             </button>
           ) : (
             <button
-              className="btn !bg-pl2 btn--small"
+              className="btn btn--small !bg-pl2"
               onClick={() => dispatch(toggleManagerRole())}
             >
               Show
@@ -231,7 +235,7 @@ const PlayerLevelAndScore = ({
 }) => {
   return (
     <div
-      className={`bg-gray00 rounded min-w-[90px] h-fit pl-2.5 py-1.5 pr-2 flex gap-1.5 justify-between items-center ${color}`}
+      className={`flex h-fit min-w-[90px] items-center justify-between gap-1.5 rounded bg-gray00 py-1.5 pl-2.5 pr-2 ${color}`}
     >
       <p className={`level text-sm font-bold`}>
         {loading ? '-' : `lvl ${level ?? '-'}`}
