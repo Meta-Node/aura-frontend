8bee1dcf1784f9001e8c35f1ea274298ed58c6aa
Ali Maktabi <maktabi876@gmail.com>
2025-02-28 13:37:17 +0330

    rollback to development build

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,6 +1,6 @@
 import { IS_PRODUCTION } from '../utils/env';
 
-export const AURA_NODE_URL_PROXY = `/auranode${!IS_PRODUCTION ? '' : '-test'}`;
+export const AURA_NODE_URL_PROXY = `/auranode${IS_PRODUCTION ? '' : '-test'}`;
 
 export const AURA_NODE_URL = IS_PRODUCTION
   ? 'https://aura-node.brightid.org'
