adecc9c164b3a70022ac05b39a25dde5cd3b9e7b
Ali Maktabi <maktabi876@gmail.com>
2025-03-01 04:40:17 +0330

    added error handling for when the app didn't started added the option to remove the cache if an upgrade occurs

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -11,6 +11,16 @@ self.addEventListener('message', (event: any) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
+self.addEventListener('install', (event: any) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName)),
+      );
+    }),
+  );
+});
+
 const manualPrecache = [{ url: 'index.html', revision: null }];
 
 precacheAndRoute([...self.__WB_MANIFEST, ...manualPrecache]);
