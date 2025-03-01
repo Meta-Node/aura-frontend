80b5240c5df7a41fa6b690c6efa25c83ee52189f
Ali Maktabi <maktabi876@gmail.com>
2025-03-01 04:59:52 +0330

    comment out previous implementation of clearing cache

--- src/prompt-sw.ts
+++ src/prompt-sw.ts
@@ -11,15 +11,15 @@ self.addEventListener('message', (event: any) => {
   if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
-self.addEventListener('install', (event: any) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => caches.delete(cacheName)),
-      );
-    }),
-  );
-});
+// self.addEventListener('install', (event: any) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => caches.delete(cacheName)),
+//       );
+//     }),
+//   );
+// });
 
 const manualPrecache = [{ url: 'index.html', revision: null }];
 
