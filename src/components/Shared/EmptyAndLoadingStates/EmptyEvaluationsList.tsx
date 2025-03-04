d7e4d3aa1c317b79f203179fe7890752a32b222a
Ali Maktabi <maktabi876@gmail.com>
2025-03-04 15:22:20 +0330

    fixed titles on not finding an evaluation or activity

--- src/components/Shared/EmptyAndLoadingStates/EmptyEvaluationsList.tsx
+++ src/components/Shared/EmptyAndLoadingStates/EmptyEvaluationsList.tsx
@@ -1,3 +1,6 @@
+import useViewMode from '@/hooks/useViewMode';
+import { toTitleCase } from '@/utils/text';
+
 export const EmptyEvaluationsList = ({
   clearFilter,
   hasFilter,
@@ -5,17 +8,21 @@ export const EmptyEvaluationsList = ({
   clearFilter: () => void;
   hasFilter: boolean;
 }) => {
+  const { currentEvaluationCategory } = useViewMode();
+
   return (
-    <div className="flex flex-col items-center justify-center py-10 px-2 gap-3">
+    <div className="flex flex-col items-center justify-center gap-3 px-2 py-10">
       <img
         src="/assets/images/Shared/no-evidence-found.svg"
         alt="Empty State"
-        className="w-10 h-8"
+        className="h-8 w-10"
       />
-      <h2 className="text-lg font-medium text-white">No Evaluations found</h2>
+      <h2 className="text-lg font-medium text-white">
+        No {toTitleCase(currentEvaluationCategory)}s found
+      </h2>
       {hasFilter && (
         <p
-          className="text-center text-white underline cursor-pointer"
+          className="cursor-pointer text-center text-white underline"
           onClick={clearFilter}
         >
           Reset view to default
