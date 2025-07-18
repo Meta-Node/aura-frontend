e482747d2b1706c85a2358b1a55909787510feac
alima <maktabi876@gmail.com>
2025-07-18 20:21:53 +0330

    chore: set the periodic interval notification fetch task to every 5 minutes

--- src/components/Shared/UpdatePrompt.tsx
+++ src/components/Shared/UpdatePrompt.tsx
@@ -18,7 +18,7 @@ const UpdatePrompt = () => {
 
       if (r && 'periodicSync' in r) {
         (r.periodicSync as any)?.register('periodic-task', {
-          minInterval: 24 * 60 * 60 * 1000,
+          minInterval: 5 * 60 * 1000,
         });
 
         Notification.requestPermission().then(() =>
